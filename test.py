48f461b17785cf1cd796758c5c2a9fc8bf8483f7
Yuan Qing Yeoh <yuanqingyeoh1999@gmail.com>
2022-01-07 00:36:25 +0800

    update plotting

--- test.py
+++ test.py
@@ -4,30 +4,41 @@ import numpy as np
 import skfuzzy as fuzz
 
 fuzz = Fuzzy()
-ext0 = fuzz.get_extension(10, 11, 0)
+ext0 = fuzz.get_extension(1, 0, 0)
 print(ext0)
 
-plt.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_few, 'r', linewidth=2, label='few')
-plt.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_small, 'g', linewidth=2, label='small')
-plt.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_medium, 'b', linewidth=2, label='medium')
-plt.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_many, 'y', linewidth=2, label='many')
-plt.legend()
-plt.xticks(np.arange(0, 17, 1))
-plt.show()
+output = []
 
-plt.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_few, 'r', linewidth=2, label='few')
-plt.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_small, 'g', linewidth=2, label='small')
-plt.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_medium, 'b', linewidth=2, label='medium')
-plt.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_many, 'y', linewidth=2, label='many')
-plt.legend()
-plt.xticks(np.arange(0, 17, 1))
-plt.show()
+# x1 = np.arange(0, 13, 1)
+# x2 = np.arange(0, 35, 1)
 
-plt.plot(fuzz.x_extension, fuzz.extension_zero, 'r', linewidth=2, label='zero')
-plt.plot(fuzz.x_extension, fuzz.extension_short, 'g', linewidth=2, label='short')
-plt.plot(fuzz.x_extension, fuzz.extension_medium, 'b', linewidth=2, label='medium')
-plt.plot(fuzz.x_extension, fuzz.extension_long, 'y', linewidth=2, label='long')
-plt.legend()
-plt.xticks(np.arange(-9, 21, 1))
-plt.show()
+# for green in x1:
+#     for red in x2:
+#         output.append([green, red, fuzz.get_extension(green, red, 0)])
 
+# arr = np.array(output)
+# np.savetxt("test_output.csv", arr, delimiter=",")
+
+fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize = [6.4, 8.5])
+
+ax1.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_few, 'r', linewidth=2, label='few')
+ax1.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_small, 'g', linewidth=2, label='small')
+ax1.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_medium, 'b', linewidth=2, label='medium')
+ax1.plot(fuzz.x_arriving_green_light, fuzz.arriving_green_light_many, 'y', linewidth=2, label='many')
+ax1.legend()
+ax1.set_xticks(np.arange(0, 13, 1))
+
+ax2.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_few, 'r', linewidth=2, label='few')
+ax2.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_small, 'g', linewidth=2, label='small')
+ax2.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_medium, 'b', linewidth=2, label='medium')
+ax2.plot(fuzz.x_behind_red_light, fuzz.behind_red_light_many, 'y', linewidth=2, label='many')
+ax2.legend()
+ax2.set_xticks(np.arange(0, 35, 2))
+
+ax3.plot(fuzz.x_extension, fuzz.extension_zero, 'r', linewidth=2, label='zero')
+ax3.plot(fuzz.x_extension, fuzz.extension_short, 'g', linewidth=2, label='short')
+ax3.plot(fuzz.x_extension, fuzz.extension_medium, 'b', linewidth=2, label='medium')
+ax3.plot(fuzz.x_extension, fuzz.extension_long, 'y', linewidth=2, label='long')
+ax3.legend()
+ax3.set_xticks(np.arange(-9, 10, 1))
+plt.show()
