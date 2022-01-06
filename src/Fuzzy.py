60da43848510e4947ef048a33485913d49e47c8a
bjc1999 <45304428+bjc1999@users.noreply.github.com>
2022-01-07 01:00:45 +0800

    edit rule

--- src/Fuzzy.py
+++ src/Fuzzy.py
@@ -51,33 +51,29 @@ class Fuzzy:
         
         ## New rules:
         # Rule 1: If Arrival is few then Extension is zero.
-        # Rule 2: If Arrival is small AND Queue is (few OR small) then Extension is medium. // I changed it to short.
-        # Rule 3: If Arrival is small AND Queue is (medium OR many) then Extension is short.
-        # Rule 4: If Arrival is medium AND Queuse is (few OR small) then Extension is long.
-        # Rule 5: If Arrival is medium AND Queue is medium then Extension is medium.
-        # Rule 6: If Arrival is medium AND Queue is many then Extension is short.
-        # Rule 7: If Arrival is many AND Queuse is (few OR small OR medium) then Extension is long.
-        # Rule 8: If Arrival is many AND Queue is many then Extension is medium.      
+        # Rule 2: If Arrival is small then Extension is short.
+        # Rule 3: If Arrival is medium AND Queuse is (few OR small) then Extension is long.
+        # Rule 4: If Arrival is medium AND Queue is medium then Extension is medium.
+        # Rule 5: If Arrival is medium AND Queue is many then Extension is short.
+        # Rule 6: If Arrival is many AND Queuse is (few OR small OR medium) then Extension is long.
+        # Rule 7: If Arrival is many AND Queue is many then Extension is medium.      
         
         rule1 = arriving_green_light_level_few
-        rule2 = np.fmin(arriving_green_light_level_small,
+        rule2 = arriving_green_light_level_small
+        rule3 = np.fmin(arriving_green_light_level_medium,
                         np.fmax(behind_red_light_level_few, behind_red_light_level_small))
-        rule3 = np.fmin(arriving_green_light_level_small,
-                        np.fmax(behind_red_light_level_medium, behind_red_light_level_many))
-        rule4 = np.fmin(arriving_green_light_level_medium,
-                        np.fmax(behind_red_light_level_few, behind_red_light_level_small))
-        rule5 = np.fmin(arriving_green_light_level_medium, behind_red_light_level_medium)
-        rule6 = np.fmin(arriving_green_light_level_medium, behind_red_light_level_many)
-        rule7 = np.fmin(arriving_green_light_level_many,
+        rule4 = np.fmin(arriving_green_light_level_medium, behind_red_light_level_medium)
+        rule5 = np.fmin(arriving_green_light_level_medium, behind_red_light_level_many)
+        rule6 = np.fmin(arriving_green_light_level_many,
                         np.fmax(behind_red_light_level_few, np.fmax(behind_red_light_level_small, behind_red_light_level_medium)))
-        rule8 = np.fmin(arriving_green_light_level_many, behind_red_light_level_many)
+        rule7 = np.fmin(arriving_green_light_level_many, behind_red_light_level_many)
 
-        #fire = [rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8]
+        #fire = [rule1, rule2, rule3, rule4, rule5, rule6, rule7]
         
         extension_activation_zero = np.fmin(rule1, self.extension_zero)
-        extension_activation_short = np.fmin(np.fmax(rule3, rule6), self.extension_short)
-        extension_activation_medium = np.fmin(np.fmax(rule2, np.fmax(rule5, rule8)), self.extension_medium)
-        extension_activation_long = np.fmin(np.fmax(rule4, rule7), self.extension_long)
+        extension_activation_short = np.fmin(np.fmax(rule2, rule5), self.extension_short)
+        extension_activation_medium = np.fmin(np.fmax(rule4, rule7), self.extension_medium)
+        extension_activation_long = np.fmin(np.fmax(rule3, rule6), self.extension_long)
 
         aggregated = np.fmax(extension_activation_zero, np.fmax(extension_activation_short,
                                                                 np.fmax(extension_activation_medium,
